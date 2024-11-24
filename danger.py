a84443ebf8e436d890fa0b2351d9d4ce437833da
sachinopboy2 <sachin25u7@gmail.com>
2024-11-24 13:27:58 +0530

    Update danger.py

--- danger.py
+++ danger.py
@@ -2,8 +2,8 @@ import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = 'BOT_TOKEN'
-ADMIN_USER_ID = 123456789
+TELEGRAM_BOT_TOKEN = '7874855079:AAEqy-YCbzHqTskBF5TtAEtlOF6lr-JiB6w'
+ADMIN_USER_ID = 7282263969
 USERS_FILE = 'users.txt'
 attack_in_progress = False
 
